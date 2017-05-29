d40c246265bcb8aa9be7160daf347c4fcfe13575
thomasdola <thomasdolar@gmail.com>
2017-05-29 07:02:59 +0000

    initial commit

---
+++ lib/birdy.ex
@@ -0,0 +1,18 @@
+defmodule Birdy do
+  @moduledoc """
+  Documentation for Birdy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Birdy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
