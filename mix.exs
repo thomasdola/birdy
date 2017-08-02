274b0d2424639837e1ad8b2463f236bc333b2b0f
thomasdola <thomasdolar@gmail.com>
2017-08-02 03:41:48 +0000

    change version to 0.0.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Birdy.Mixfile do
 
   def project do
     [app: :birdy,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
